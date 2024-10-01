5f8171f87320617c17b2f28bbad1b2487e55ebeb
Xu Gao <xgao106@uottawa.ca>
2024-10-01 17:08:46 -0400

    admin interface

--- student_management/studentmanage/views.py
+++ student_management/studentmanage/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Student
+from django.urls import reverse_lazy
 
-# Create your views here.
+class StudentListView(ListView):
+    model = Student
+    template_name = 'studentmanage/student_list.html'
+    context_object_name = 'students'
+
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = 'studentmanage/student_detail.html'
+    context_object_name = 'student'
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm  
+    template_name = 'studentmanage/student_form.html'
+    success_url = reverse_lazy('studentmanage:student_list')
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm  
+    template_name = 'studentmanage/student_form.html'
+    success_url = reverse_lazy('studentmanage:student_list')
